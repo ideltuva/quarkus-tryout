9f4f1e995453586995cf177f73a3510b08345cec
Deltuva <S89387@corp1.ad1.seb.net>
2020-02-17 12:48:28 +0200

    kafka topic renamed

--- message-kafka/src/main/java/api/quarkus/KafkaMessage.java
+++ message-kafka/src/main/java/api/quarkus/KafkaMessage.java
@@ -20,7 +20,7 @@ public class KafkaMessage {
     private ExecutorService queue = Executors.newSingleThreadExecutor();
 
     @Incoming("messages")
-    @Outgoing("messageQueue")
+    @Outgoing("message-queue")
     public CompletionStage<MessageQueue> process(Message message) {
         return CompletableFuture.supplyAsync(() -> {
             MessageQueue messageQueue = prepare(message);
